6e7f16e03af8e5b10d89b621b478f798ad5a50da
emileebogdan <emileebogdan@gmail.com>
2022-11-28 10:32:12 -0700

    manager content

--- content/Manager.js
+++ content/Manager.js
@@ -5,11 +5,13 @@ class Manager extends Employee {
         super (name, id, email); 
         
         this.officeNumber = officeNumber; 
+        this.role = 'Manager'
+    }
+// write additonal method to return office number 
+    getOfficeNumber () {
+        return this.officeNumber
     }
 
-    getRole () {
-        return "Manager";
-    }
 }
 
 module.exports = Manager; 
